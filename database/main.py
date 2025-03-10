9d5cb56ca891eae033174a12b9494af44163e327
Vincent Marcel Santos <vcsantos4@up.edu.ph>
2025-03-10 10:02:01 +0800

    added filtering based on interactors for database

--- database/main.py
+++ database/main.py
@@ -1,37 +1,27 @@
 import pandas as pd
 
+NAME_A = "Systematic Name Interactor A"
+NAME_B = "Systematic Name Interactor B"
+EXPERIMENT = "Experimental System"
+EXPERIMENT_TYPE = "Experimental System Type"
+SCORE = "Score"
+
 class Database:
-    def __init__(self, path, sep='\t'):
+    def __init__(self, path:str, sep:str='\t'):
         self.df = pd.read_csv(path, sep=sep)
 
+        to_retain = [
+            NAME_A,
+            NAME_B,
+            EXPERIMENT,
+            EXPERIMENT_TYPE,
+            SCORE
+        ]
 
-df = pd.read_csv("biogrid-sample.txt", sep='\t')
-# df = pd.read_csv("BIOGRID-ALL-4.4.239.tab3.txt", sep='\t')
-to_list = ["Synonyms Interactor A", "Synonyms Interactor B" ]
-print(df[to_list])
+        self.df = self.df[to_retain]
 
-for i in to_list:
-    df[i] = df[i].apply(lambda x: x[0:-1].split('|'))
+    def filter_interactions(self, a: str, b: str) -> pd.DataFrame:
+        # if a < b:
+        #     a,b = b,a
 
-
-find = "YAL001C"
-
-#print(df.columns)
-
-#interest = ['Systematic Name Interactor A',
-#       'Systematic Name Interactor B', 'Official Symbol Interactor A',
-#       'Official Symbol Interactor B', 'Synonyms Interactor A',
-#       'Synonyms Interactor B',]
-#
-#for i in interest:
-#    print(df[i].head())
-
-print(df[to_list])
-
-#print(df.loc[find in df[to_list[0]]])
-#print(df.loc[find in df[to_list[1]]])
-
-# for col in df.columns:
-# 	print(f"{col = }")
-# 	print(df[col].head(3))
-# 	print("\n\n\n")
+        return self.df[(self.df[NAME_A] == a) & (self.df[NAME_B == b])]
