3b92835943ec3291054b7dabca98efb9dedf37a1
scl <yipee@DESKTOP-U999QP6.>
2025-04-23 13:41:11 +0800

    interface with argparse

--- main.py
+++ main.py
@@ -1,8 +1,69 @@
+# from graph import Graph
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog = "SCL 2024-2025 Thesis",
+        description="PPIN Reweighter using Topology and Experimenal Source")
+
+    parser.add_argument("--input_graph", "-I", required=True, help="path of input graph")
+    parser.add_argument("--database_path", "-D", required=True, help="path to BioGRID database")
+    parser.add_argument("--depth_from", "-df", default=0, type=int, help="include starting from to depth n (must be geq than 0)")
+    parser.add_argument("--depth_to", "-dt", default=0, type=int, help="include up to depth n (must be geq than 0)")
+    parser.add_argument("--verbose", "-v", action="store_true")
+    parser.add_argument("--path", "-p", help="path to save reweighted graph")
+
+
+    args = parser.parse_args()
+
+    # print(args.input_graph, args.database_path, args.depth, args.verbose, args.path)
+    from graph import Graph
+    G = Graph(dataset_path=args.input_graph, database_path=args.database_path)
+
+    for depth in range(args.depth_from, args.depth_to+1):
+        path = args.path
+        if path:
+            inp = args.input_graph
+            sep = ""
+            if "\\\\" in inp:
+                sep = "\\\\"
+            elif "\\" in inp:
+                sep = "\\"
+            else:
+                sep = "/"
+
+            file = inp.split(sep)[-1].split(".")
+            name = file[0]
+            ext = file[1]
+    
+
+            path += f"{sep}{name}v{depth}.{ext}"
+
+        print(f"Running Algoritm on {args.input_graph} with {depth = }")
+        G.set_depth(depth)
+        G.reweight_chua(path, args.verbose)
+        print("done")
+
+
+
+
+
+"""
+scratch
 from graph import Graph
+
+
+G = Graph("./dataset.txt")
+
+print(G.N("YAL033W"))
+
+G.visualize()
+
+
+-----
 
 TARGET = "Collins"
 trials = 4
-
 
 def ppath(dataset, trialno = None):
     p = "./datasets/"
@@ -20,17 +81,4 @@ for i in range(trials):
     G.set_depth(run)
     G.reweight_with_reliability(ppath(TARGET, run),True)
     print("Finished run", run)
-
-
-
-"""
-scratch
-from graph import Graph
-
-
-G = Graph("./dataset.txt")
-
-print(G.N("YAL033W"))
-
-G.visualize()
 """
